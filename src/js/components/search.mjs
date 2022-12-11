3dec9ce553c291f76f6bd0043ab47bf3f198d8bf
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-12-11 15:11:20 +0100

    add lastbidamount to search resalt

--- src/js/components/search.mjs
+++ src/js/components/search.mjs
@@ -1,5 +1,6 @@
 import { getAllListingUrl } from "../url.mjs";
 import { timeformat } from "./timeformat.mjs";
+import { apiCall } from "./apiCall.mjs";
 const searchBar = document.querySelector(".search__bar");
 const container = document.querySelector(".container__card");
 
@@ -15,13 +16,8 @@ searchBar.addEventListener("keyup", (e) => {
 
 export async function searchApiCall() {
    try {
-      const response = await fetch(getAllListingUrl, {
-         method: "GET",
-         headers: {
-            "Content-Type": "application/json",
-         },
-      });
-      searchArray = await response.json();
+      searchArray = await apiCall(getAllListingUrl, "GET");
+      // console.log(searchArray);
    } catch (error) {
       console.log("error search api call");
    }
@@ -37,30 +33,61 @@ function showSearchResult(result) {
             secondRemaining,
          ] = timeformat(`${element.endsAt}`);
 
-         return ` <div class="col-12 col-md-6 col-lg-4 mb-5">
-                  <div class="card position-relative shadow h-100">
-                  <a href="/specificprod.html?id=${element.id}">
-                     <img class="card-img-top card__size--img"
-                     src="${element.media[0]}"
-                     onerror="this.src = '../image/paul-volkmer-qVotvbsuM_c-unsplash.jpg';"
-                     alt="image of the product"/>
-                  </a>
-                  <h4 class="card-text position-absolute top-0 start-0 p-1 bg-light">                     
-                  <span class="me-1">${daysRemaining}d </span>                    
-                  <span class="me-1">${hoursRemaining}h </span>                    
-                  <span class="me-1">${minuteRemaining}m </span>                    
-                  <span>${secondRemaining}s </span>
-                  </h4>
-                  <div class="card-body position-relative">
-                  <a href="#"
-                  class="btn btn-secondary btn__card position-absolute top-0 start-50 translate-middle rounded-circle shadow"
-                  ><span class="position-absolute top-50 start-50 translate-middle fw-bold fs-4"> BID</span>
-                  </a>
-                  <h3 class="card-title text-center mt-4">${element.title}</h3>
-                  <p class="card-text text-center">269</p>
-                  </div>
-                  </div>
-                  </div> `;
+         console.log(element.bids);
+         if (element.bids.length === 0) {
+            return ` <div class="col-12 col-md-6 col-lg-4 mb-5">
+                     <div class="card position-relative shadow h-100">
+                     <a href="/specificprod.html?id=${element.id}">
+                        <img class="card-img-top card__size--img"
+                        src="${element.media[0]}"
+                        onerror="this.src = '../image/paul-volkmer-qVotvbsuM_c-unsplash.jpg';"
+                        alt="image of the product"/>
+                     </a>
+                     <h4 class="card-text position-absolute top-0 start-0 p-1 bg-light">                     
+                     <span class="me-1">${daysRemaining}d </span>                    
+                     <span class="me-1">${hoursRemaining}h </span>                    
+                     <span class="me-1">${minuteRemaining}m </span>                    
+                     <span>${secondRemaining}s </span>
+                     </h4>
+                     <div class="card-body position-relative">
+                     <a href="#"
+                     class="btn btn-secondary btn__card position-absolute top-0 start-50 translate-middle rounded-circle shadow"
+                     ><span class="position-absolute top-50 start-50 translate-middle fw-bold fs-4"> BID</span>
+                     </a>
+                     <h3 class="card-title text-center mt-4">${element.title}</h3>
+                     <p class="card-text text-center">0</p>
+                     </div>
+                     </div>
+                     </div> `;
+         } else {
+            const lastbid = element.bids[element.bids.length - 1];
+            const lastBidAmount = lastbid.amount;
+
+            return ` <div class="col-12 col-md-6 col-lg-4 mb-5">
+                     <div class="card position-relative shadow h-100">
+                     <a href="/specificprod.html?id=${element.id}">
+                        <img class="card-img-top card__size--img"
+                        src="${element.media[0]}"
+                        onerror="this.src = '../image/paul-volkmer-qVotvbsuM_c-unsplash.jpg';"
+                        alt="image of the product"/>
+                     </a>
+                     <h4 class="card-text position-absolute top-0 start-0 p-1 bg-light">                     
+                     <span class="me-1">${daysRemaining}d </span>                    
+                     <span class="me-1">${hoursRemaining}h </span>                    
+                     <span class="me-1">${minuteRemaining}m </span>                    
+                     <span>${secondRemaining}s </span>
+                     </h4>
+                     <div class="card-body position-relative">
+                     <a href="#"
+                     class="btn btn-secondary btn__card position-absolute top-0 start-50 translate-middle rounded-circle shadow"
+                     ><span class="position-absolute top-50 start-50 translate-middle fw-bold fs-4"> BID</span>
+                     </a>
+                     <h3 class="card-title text-center mt-4">${element.title}</h3>
+                     <p class="card-text text-center">${lastBidAmount}</p>
+                     </div>
+                     </div>
+                     </div> `;
+         }
       })
       .join("");
 
